4dba4aced6e21dbd24e87e3d0c30cc5e274cc819
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-06-23 14:45:51 -0400

    update server

--- src/app.js
+++ src/app.js
@@ -23,6 +23,10 @@ mongoose.Promise = global.Promise
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 const DB_URL = process.env.MONGO_URL;
 
 // Connect to mongoose
@@ -32,8 +36,6 @@ mongoose.connect(DB_URL, { useNewUrlParser: true }, (err) => {
     return console.log('Connection Successful')
 })
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 //middleware to utilize routes
 app.use('/api/patient', patientRoutes);
